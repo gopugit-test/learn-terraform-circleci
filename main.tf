1d6b9c872f326e2c051b86c5f512c9e1fa8417d1
gopugit-test <127150073+gopugit-test@users.noreply.github.com>
2024-03-17 13:06:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,14 +30,6 @@ resource "aws_s3_bucket_public_access_block" "app" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "app" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.app,
-    aws_s3_bucket_public_access_block.app,
-  ]
 
-  bucket = aws_s3_bucket.app.id
-  acl    = "public-read"
-}
 
 
