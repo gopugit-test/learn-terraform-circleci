c731f486a017f24c2be4092c284cf5b0bda3b8e2
gopugit-test <127150073+gopugit-test@users.noreply.github.com>
2024-03-17 12:38:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-terraform {
-required_providers {
-aws = {
-source = "hashicorp/aws"
+provider "aws" {
+  region = var.region
 
-   }        
+  default_tags {
+    tags = {
+      hashicorp-learn = "circleci"
+    }
   }
 }
 
@@ -45,4 +46,22 @@ resource "aws_s3_bucket_acl" "app" {
   acl    = "public-read"
 }
 
+resource "aws_s3_bucket_website_configuration" "app" {
+  bucket = aws_s3_bucket.app.bucket
 
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "app" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.app.id
+  content      = file("./assets/index.html")
+  content_type = "text/html"
+}
