808785e38950e3a954514638f2344c2f9b95eb71
gopugit-test <127150073+gopugit-test@users.noreply.github.com>
2024-03-17 12:46:51 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,8 +3,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.21.0"
-      aws-access-key-id: AWS_ACCESS_KEY
-      aws-secret-access-key: AWS_ACCESS_SECRET
+
     }
   }
 
